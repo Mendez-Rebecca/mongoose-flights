147032ba1a0e14498ae0f154b3b18b3b98d8f240
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-19 13:28:45 -0700

    edit the schema

--- models/flight.js
+++ models/flight.js
@@ -3,32 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: {
-        type: String,
-        enum: ['American', 'Southwest', 'United'],
-        default: null
-    },
-    airport: {
-        type: String,
-        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
-        default: 'DEN'
-    },
-    flightNo: {
-        type: Number,
-        validation: function(value) {
-            return value >= 10 && value <= 9999
-        },
-        default: null
-    },
-    departs: {
-        type: Date,
-        validation: null,
-        default: function() {
-            const addOneYear = new Date();
-            addOneYear.setFullYear(addOneYear.getFullYear() + 1);
-            return addOneYear;
-        }
-    }
+    airline: String,
+    airport: String,
+    flightNo: Number,
+    departs: Date
 });
 
 module.exports = mongoose.model('Flight', flightSchema)
