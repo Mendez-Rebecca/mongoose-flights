f973f3b4cfa5bad8612646a2d20e4e07bc91004b
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-19 13:44:47 -0700

    add flights table

--- routes/flights.js
+++ routes/flights.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const flightsCtrl = require('../controllers/flights');
+
+//GET flights
+router.get('/', flightsCtrl.index);
 
 module.exports = router;
