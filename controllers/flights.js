a27f44b212a1eb3681c092e18eb58cf4a2078d5f
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-19 15:07:48 -0700

    add new and create functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,10 +1,26 @@
-const Flights = require('../models/flight');
+const Flight = require('../models/flight');
 
 module.exports = {
-    index
+    index,
+    new: newFlight,
+    create
 }
 
 async function index(req, res) {
     const flights = await Flight.find({});
     res.render('flights/index', { flights });
+}
+
+function newFlight(req, res) {
+    res.render('flights/new', { errorMsg: '' });
+}
+
+async function create(req, res) {
+    try {
+        await Flight.create(req.body);
+        res.redirect('/flights');
+    } catch (err) {
+        console.log(err);
+        res.render('flights/new', { errorMsg: err.message});
+    }
 }
