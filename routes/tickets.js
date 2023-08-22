ca6685974250a267c1e17065987868bca8683053
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-22 14:15:37 -0700

    create ticket routes

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ticketsCtrl = require('../controllers/tickets');
 
-
+//add a new
+router.get('/tickets/new', ticketsCtrl.new);
+//add a create
+router.post('/tickets', ticketsCtrl.create);
+//associate a ticket with a flight
+router.post('/flights/:id/tickets', ticketsCtrl.addToFlight);
 
 
 module.exports = router;
