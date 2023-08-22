d8aefb6da479e9e0fd270c329c1d5b03a2cc062a
Rebecca <mendez_elizabeth88@hotmail.com>
2023-08-22 14:45:43 -0700

    add tickets controller functions

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -8,13 +8,22 @@ module.exports = {
 }
 
 async function newTicket(req, res) {
-
+    const tickets = await Ticket.find({}).sort('seat');
+    res.render('tickets/new', { title: 'Add Ticket', tickets});
 }
 
 async function create(req, res) {
-
+    try {
+        await Ticket.create(req.body);
+    } catch (err) {
+        console.log(err);
+    }
+    res.redirect('/tickets/new');
 }
 
 async function addToFlight(req, res) {
-
+    const flight = await Ticket.findById(req.params.id);
+    flight.flight.push(req.body.ticketId);
+    await flight.save();
+    res.redirect(`/flights/${movie._id}`);
 }
